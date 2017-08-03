bd1a3f5ae25a44bbc8f981cddd67d68fbaaffcd6
MlWoo <megnlin.wu@intel.com>
2017-08-03 11:15:33 +0800

    random.c para opt further

--- src/Random.c
+++ src/Random.c
@@ -8,39 +8,38 @@ void MKLNN_(Dropout_updateOutput)(
   THTensor *output,
   double p)
 {
-    THArgCheck(p >= 0 && p <= 1, 1, "must be >= 0 and <= 1");
+  THArgCheck(p >= 0 && p <= 1, 1, "must be >= 0 and <= 1");
 
-    struct timeval start;
-    gettimeofday(&start,NULL);
-    long seed = start.tv_sec * 1000 + (double)start.tv_usec/1000;
+  struct timeval start;
+  gettimeofday(&start,NULL);
+  long seed = start.tv_sec * 1000 + (double)start.tv_usec/1000;
 
-    RNG  rng = RNGInit(seed);
-    unsigned long seedNew = RandInt(&rng);
-    int n = THTensor_(nElement)(input);
-    real *r = THTensor_(data)(output);
-    int nthr = omp_get_max_threads();
-    int *tmp = (int*)malloc(n*sizeof(int));
-    # pragma omp parallel num_threads(nthr)
-    {
+  RNG  rng = RNGInit(seed);
+  unsigned long seedNew = RandInt(&rng);
+  int n = THTensor_(nElement)(input);
+  real *r = THTensor_(data)(output);
+  int nthr = omp_get_max_threads();
+  int *tmp = (int*)malloc(n*sizeof(int));
+  #pragma omp parallel num_threads(nthr) 
+  {
+    int i;
     const int ithr = omp_get_thread_num();
     const int avg_amount = (n + nthr - 1) / nthr;
-    const int my_offset = ithr * avg_amount;
-    const int my_amount = min(my_offset + avg_amount, n) - my_offset;
+    const int seg_offset = ithr * avg_amount;
+    const int seg_last_index_tmp = seg_offset + avg_amount;
+    const int seg_last_index = seg_last_index_tmp <= n ? seg_last_index_tmp:n;
+    const int seg_amount = seg_last_index - seg_offset;
          
-    if (my_amount > 0) {
+    if (seg_amount > 0) {
       VSLStreamStatePtr stream;
       vslNewStream(&stream, VSL_BRNG_MCG31, seedNew);
-      vslSkipAheadStream(stream, my_offset);
-      viRngBernoulli(VSL_RNG_METHOD_BERNOULLI_ICDF, stream, my_amount,
-        tmp + my_offset, p);
+      vslSkipAheadStream(stream, seg_offset);
+      viRngBernoulli(VSL_RNG_METHOD_BERNOULLI_ICDF, stream, seg_amount, tmp+seg_offset, p);
       vslDeleteStream(&stream);
+      for(i = seg_offset; i < seg_last_index; i++) {
+        r[i]=tmp[i];
+      }
     }
-    }
-
-  int k;
-  for(k=0;k<n;k++)
-  {
-    r[k]=tmp[k];
   }
   free(tmp);
 }
